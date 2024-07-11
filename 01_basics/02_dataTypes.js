7da7b3906185365a755809553c7b021711a3b7ce
Harsh Mittal <139866543+harshmittal311@users.noreply.github.com>
2024-07-11 07:59:35 +0000

    data types

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,34 @@
+"use strict";  // treat all JS code as newer version
+
+// alert("hello")   we are using in nodejs , not in browser
+
+console.log(3
+  +3)  // code read ability should be higher 
+console.log("Hiest");
+
+let name = "harsh"
+let age = 22
+let isLoggedIn = false;
+
+/*
+
+ Primitive data types
+
+
+// number => 2 to power 53
+// bigint 
+// sting ->  use in "", ''
+//  boolean ->  true/false  \
+// null -> standalone value 
+// undefined ->   not assign the value
+// symbol ->  is use to find the unique ness
+
+*/
+
+console.log(typeof age);
+console.log(typeof "harsh");
+console.log (typeof null)    // type is object
+console.log (typeof undefined)  // tpye is undefined
+
+
+
