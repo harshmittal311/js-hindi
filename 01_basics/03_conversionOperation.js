8f1e0dbe55764ed8869673c06926d47bb89da689
Harsh Mittal <139866543+harshmittal311@users.noreply.github.com>
2024-07-11 17:40:42 +0000

    conersion operation

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,18 @@
+let score = 35
+
+
+console.log(typeof score);
+
+let valueinmumber = Number(score)
+console.log(typeof valueinmumber);
+console.log(valueinmumber);
+console.log(typeof NaN);
+
+
+/*
+"33"  ->  33
+"33 abc"  -> NaN
+true   ->  1;  false -> 0
+*/
+
+
