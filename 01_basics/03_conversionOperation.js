fdc9a257f0de96532a8dcf6f163fcf6141f32300
Harsh Mittal <139866543+harshmittal311@users.noreply.github.com>
2024-07-18 16:16:09 +0000

    conversion operation

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,12 +1,12 @@
 let score = 35
 
 
-console.log(typeof score);
+//console.log(typeof score);
 
 let valueinmumber = Number(score)
-console.log(typeof valueinmumber);
-console.log(valueinmumber);
-console.log(typeof NaN);
+//console.log(typeof valueinmumber);
+//console.log(valueinmumber);
+//console.log(typeof NaN);
 
 
 /*
@@ -14,5 +14,18 @@ console.log(typeof NaN);
 "33 abc"  -> NaN
 true   ->  1;  false -> 0
 */
+let isLoggedIn = "harsh"
+
+let bolleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(bolleanIsLoggedIn)
+
+// 1 --> true
+// 0 --> false
+// "harsh" --> true
+// " " --> false
+
+
+let someNumber = 35
+console.log(someNumber)
 
 
