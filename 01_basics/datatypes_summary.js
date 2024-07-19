a2f569d5b090120ba8c71796d1f948bbb662468c
Harsh Mittal <139866543+harshmittal311@users.noreply.github.com>
2024-07-19 16:51:51 +0000

    data type summary

---
+++ 01_basics/datatypes_summary.js
@@ -0,0 +1,24 @@
+// Primitive
+
+// 7 types
+/*
+string
+number
+boolean
+null
+undefined
+symbol------ for uniquee
+bigInt
+
+*/
+
+// Reference (non primitive)
+/*
+array
+object
+function
+
+ */
+
+const score = 100
+console.log(score);
