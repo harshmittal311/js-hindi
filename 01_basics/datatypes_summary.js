61d9ff68657e054474f96977f80ecbf43495ca2e
Harsh Mittal <139866543+harshmittal311@users.noreply.github.com>
2024-07-20 02:42:30 +0000

    data types summary

--- 01_basics/datatypes_summary.js
+++ 01_basics/datatypes_summary.js
@@ -11,6 +11,24 @@ symbol------ for uniquee
 bigInt
 
 */
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('1234')
+const anotherId = Symbol('1234')
+
+//console.log(id === anotherId);
+
+const bigNumber = 34567891234568547n
+
+
+console.log(typeof userEmail)
+
+//console.log(score);
 
 // Reference (non primitive)
 /*
@@ -20,5 +38,17 @@ function
 
  */
 
-const score = 100
-console.log(score);
+const heros = ["shaktiman", "naagraj", "doga"]  // array always write in brackets....
+
+let myobj = {
+    name: "Harsh",
+    age: 22,
+}
+
+const myFunction = function(){
+console.log("Hello World")
+}
+
+
+
+console.log(typeof myFunction)
