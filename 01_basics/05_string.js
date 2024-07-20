b0b0c97283a0a9fd654d5170c2efeaaa50294c4f
Harsh Mittal <139866543+harshmittal311@users.noreply.github.com>
2024-07-20 11:14:13 +0000

    string

---
+++ 01_basics/05_string.js
@@ -0,0 +1,8 @@
+const name = "Harsh"
+const repoCount = 50
+
+//console.log(name +  repoCount + " Value")
+
+console.log(`Hello my Name is ${name} and my repo count is ${repoCount}`+ " Value")
+
+const game 
