b070e50d37e98394482a197ed50c3ca96be60c78
Harsh Mittal <139866543+harshmittal311@users.noreply.github.com>
2024-07-22 07:14:33 +0000

    Numbers and maths

---
+++ 01_basics/06_nums_and_maths.js
@@ -0,0 +1,15 @@
+const score = 200
+//console.log(score)
+
+const balance = new Number(100)
+//console.log(balance)
+
+console.log(balance.toString().length)
+console.log(balance.toFixed(2))
+//console.log(typeof balance)
+
+const otherNumber =125.965859
+console.log(otherNumber.toPrecision(3))
+
+const _numbers = 100000000
+console.log(_numbers.toLocaleString('en-in'))
