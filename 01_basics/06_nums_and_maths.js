34f3f652063eac99b544b71943e8fabb07e84251
Harsh Mittal <139866543+harshmittal311@users.noreply.github.com>
2024-07-22 17:21:59 +0000

    numbers and maths

--- 01_basics/06_nums_and_maths.js
+++ 01_basics/06_nums_and_maths.js
@@ -1,15 +1,40 @@
+// +++++++++++++++++++++++++  Nums  ++++++++++++++++++++++++++++++//
+
 const score = 200
 //console.log(score)
 
 const balance = new Number(100)
 //console.log(balance)
 
-console.log(balance.toString().length)
-console.log(balance.toFixed(2))
+//console.log(balance.toString().length)
+//console.log(balance.toFixed(2))
 //console.log(typeof balance)
 
 const otherNumber =125.965859
-console.log(otherNumber.toPrecision(3))
+//console.log(otherNumber.toPrecision(3))
 
 const _numbers = 100000000
-console.log(_numbers.toLocaleString('en-in'))
+//console.log(_numbers.toLocaleString('en-in'))
+
+
+
+
+// +++++++++++++++++++++++++++   Maths  ++++++++++++++++++++++++++//
+
+//console.log(Math)
+
+//console.log(Math.abs(-4))
+//console.log(Math.round(4.7))
+//console.log(Math.ceil(4.2))    // ON using ceil gives upper value
+//console.log(Math.ceil(4.8))
+//console.log(Math.floor(4.2))   // on using floor gives small value
+//console.log(Math.floor(4.6))
+//console.log(Math.pow(5))  
+//console.log(Math.sqrt(4))
+
+console.log(Math.random())
+console.log(Math.floor(Math.random()*10)+1)
+
+const min = 10
+const max = 20
+console.log(Math.floor(Math.random()*(max - min +1))+min)
