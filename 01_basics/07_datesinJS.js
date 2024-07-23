4119ef5a1a43c23c14c6cae6c0b399a3d8f06696
Harsh Mittal <139866543+harshmittal311@users.noreply.github.com>
2024-07-23 08:07:03 +0000

    dates

--- 01_basics/07_datesinJS.js
+++ 01_basics/07_datesinJS.js
@@ -1,6 +1,11 @@
 //   Dates
 
 
-
 let myDate = new Date()
 console.log(myDate.toString())
+console.log(myDate.toDateString())
+console.log(myDate.toLocaleString())
+console.log(myDate.toLocaleDateString())
+console.log(myDate.toLocaleTimeString())
+console.log(myDate.getDate())
+console.log(myDate.getDay())
