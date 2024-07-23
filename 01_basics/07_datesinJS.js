fb05fa996ac770d9e3fd49c217db3ebd526570a0
Harsh Mittal <139866543+harshmittal311@users.noreply.github.com>
2024-07-23 16:49:59 +0000

    dates

--- 01_basics/07_datesinJS.js
+++ 01_basics/07_datesinJS.js
@@ -5,7 +5,15 @@ let myDate = new Date()
 console.log(myDate.toString())
 console.log(myDate.toDateString())
 console.log(myDate.toLocaleString())
-console.log(myDate.toLocaleDateString())
-console.log(myDate.toLocaleTimeString())
-console.log(myDate.getDate())
-console.log(myDate.getDay())
+//console.log(myDate.toLocaleDateString())
+//console.log(myDate.toLocaleTimeString())
+//console.log(myDate.getDate())
+//console.log(myDate.getDay())
+
+console.log(typeof myDate) //// date type is object
+
+
+let myCreateDate = new Date(2024, 6, 23)
+console.log(myCreateDate.toDateString())
+
+
