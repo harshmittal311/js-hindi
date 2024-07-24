5ddca488fd742edbdddaf810a05c9d7a90a635d3
Harsh Mittal <139866543+harshmittal311@users.noreply.github.com>
2024-07-24 03:01:21 +0000

    Array

--- 02_basics/01_array.js
+++ 02_basics/01_array.js
@@ -1,4 +1,16 @@
 //        Array
 
-const myArr = [0,1,2,3,4,5, "Harsh"]
+const myArr = [0, 1, 2, 3, 4, 5, "true" ,"Harsh",true]
+//console.log(myArr)
+const myHero = ["harsh " , "Taru"]
+//console.log(myHero)
+const myArr2 =new Array(1,2,3,4)
+//console.log(myArr2)
+
+
+// Arrayy Methods
+
+myArr.push(6)
+myArr.push(7)
+myArr.pop()
 console.log(myArr)
