e4f4eb1ff8efc84a6e5d714c6532d81c72fb3ef0
Harsh Mittal <139866543+harshmittal311@users.noreply.github.com>
2024-07-27 11:43:02 +0000

    Object

---
+++ 02_basics/04_object.js
@@ -0,0 +1,12 @@
+//const tinderuser = new Object()
+const tinderuser = {}
+
+tinderuser.id = "123abc"
+tinderuser.name = "Harsh"
+tinderuser.isLoggedIn = false
+
+//console.log(tinderuser)
+
+const regularuser = {
+    
+}
