be0a17318ff9f333391fb4d9c26611cff2f8e2a4
Harsh Mittal <139866543+harshmittal311@users.noreply.github.com>
2024-07-27 05:56:30 +0000

    Array

--- 02_basics/02_array.js
+++ 02_basics/02_array.js
@@ -5,7 +5,6 @@ const dc_hero = ["superman","flash","batman"]
 //console.log(marvel_hero)
 // push array work on same array but concat returm new array
 
-
 //const all_hero  = marvel_hero.concat(dc_hero)
 
 //console.log(all_hero)
@@ -23,6 +22,20 @@ const dc_hero = ["superman","flash","batman"]
 
 console.log(Array.isArray("harsh"))
 console.log(Array.from("harsh"))
+
+console.log(Array.from({name : "harsh"}))    // interesting factor
+
+let score1 = 100
+let score2 = 200
+let score3 = 300
+
+
+console.log(Array.of(score1,score2, score3))      
+
+// please read about isarray , from and of etc....
+
+//Harsh Mittal 
+//Taru Mittal
 
 
 
